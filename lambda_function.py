a49912af4d0881b7ea405befaf090c7f08052a52
siropaca <ryhshr3@gmail.com>
2020-09-05 19:01:23 +0900

    modified: lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,7 @@ import os
 import json
 import urllib.parse
 import boto3
+import cv2
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
@@ -9,8 +10,8 @@ def lambda_handler(event, context):
 
     file_name = 'test.jpg'
 
-    bucket_name = 'whitenote'
-    key = 'test/' + file_name
+    bucket_name = 'dex3w'
+    key = 'cv-test/' + file_name
 
     # ローカルのファイル保存先を設定
     tmp_file_path = '/tmp/' + file_name
@@ -22,19 +23,27 @@ def lambda_handler(event, context):
         # ダウンロードされているか確認
         print(os.listdir('/tmp'))
 
-        upload_key = 'test/processed-' + file_name
-        s3.upload_file(tmp_file_path, bucket_name, upload_key)
+        # 画像処理
+        orgImage = cv2.imread(tmp_file_path)
+        grayImage = cv2.cvtColor(orgImage, cv2.COLOR_RGB2GRAY)
+        gray_file_path = '/tmp/cv-test/processed-' + file_name
+        cv2.imwrite(gray_file_path, grayImage)
+        print('imwrite!: ' + gray_file_path)
+
+        # S3にアップロード
+        upload_key = 'cv-test/processed-' + file_name
+        s3.upload_file(gray_file_path, bucket_name, upload_key)
         print('Upload!: ' + upload_key)
 
     except Exception as e:
         print(e)
 
     finally:
+        # 後処理
         if os.path.exists(tmp_file_path):
             os.remove(tmp_file_path)
-            print('Removed!: ' + tmp_file_path)
+            print('Deleted!: ' + upload_key)
 
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps('Hello from Lambda!!!')
-    # }
+        if os.path.exists(gray_file_path):
+            os.remove(gray_file_path)
+            print('Deleted!: ' + gray_file_path)
